ac774e5e08521df31fad5e767c349b50a492c317
p-kordana <pkordana422@gmail.com>
2024-04-18 15:49:16 -0400

    add loader and loading element

--- src/features/menu/Menu.jsx
+++ src/features/menu/Menu.jsx
@@ -1,5 +1,21 @@
+import { useLoaderData } from "react-router-dom";
+import { getMenu } from "../../services/apiRestaurant";
+import MenuItem from "./MenuItem";
+
 function Menu() {
-  return <h1>Menu</h1>;
+  const menu = useLoaderData();
+  return (
+    <ul>
+      {menu.map((mi) => (
+        <MenuItem key={mi.id} pizza={mi} />
+      ))}
+    </ul>
+  );
+}
+
+export async function loader() {
+  const menu = await getMenu();
+  return menu;
 }
 
 export default Menu;
