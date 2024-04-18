2a0d6b6a81296c6cc9ecf9fadc5551fae8cbd24d
p-kordana <pkordana422@gmail.com>
2024-04-18 14:21:06 -0400

    apply minor fixes

--- src/features/cart/CartItem.jsx
+++ src/features/cart/CartItem.jsx
@@ -1,3 +1,10 @@
+import { formatCurrency } from "../../utils/helpers";
+import PropTypes from "prop-types";
+
+CartItem.propTypes = {
+  item: PropTypes.any,
+};
+
 function CartItem({ item }) {
   const { pizzaId, name, quantity, totalPrice } = item;
 
--- src/features/order/OrderItem.jsx
+++ src/features/order/OrderItem.jsx
@@ -1,3 +1,12 @@
+import PropTypes from "prop-types";
+import { formatCurrency } from "../../utils/helpers";
+
+OrderItem.propTypes = {
+  item: PropTypes.any,
+  isLoadingIngredients: PropTypes.bool,
+  ingredients: PropTypes.any,
+};
+
 function OrderItem({ item, isLoadingIngredients, ingredients }) {
   const { quantity, name, totalPrice } = item;
 
