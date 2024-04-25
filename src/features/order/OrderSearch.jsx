b47a22732a004a777171ec68cff0026c4abcef9f
p-kordana <pkordana422@gmail.com>
2024-04-25 14:14:08 -0400

    style ordersearch

--- src/features/order/OrderSearch.jsx
+++ src/features/order/OrderSearch.jsx
@@ -18,6 +18,7 @@ function OrderSearch() {
         placeholder="Search Order #"
         value={query}
         onChange={(e) => setQuery(e.target.value)}
+        className="w-32 rounded-lg bg-amber-100 px-3 py-1 text-sm transition-all duration-300 placeholder:text-stone-400 focus:shadow-xl focus:outline-none sm:w-48 sm:focus:w-72"
       ></input>
     </form>
   );
