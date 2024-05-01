f346fda8039c9bf75b1abfef10dbb909b8911219
p-kordana <pkordana422@gmail.com>
2024-05-01 14:26:17 -0400

    implement redux for userSlice

--- src/features/user/CreateUser.jsx
+++ src/features/user/CreateUser.jsx
@@ -1,12 +1,21 @@
 import { useState } from "react";
 import Button from "../../ui/Button";
 import Input from "../../ui/Input";
+import { useDispatch } from "react-redux";
+import { updateName } from "./userSlice";
+import { useNavigate } from "react-router-dom";
 
 function CreateUser() {
   const [username, setUsername] = useState("");
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
   function handleSubmit(e) {
     e.preventDefault();
+
+    if (!username) return;
+    dispatch(updateName(username));
+    navigate("/menu");
   }
 
   return (
@@ -25,7 +34,7 @@ function CreateUser() {
 
       {username !== "" && (
         <div>
-          <Button className="mt-3 px-3 py-2">Start ordering</Button>
+          <Button>Start ordering</Button>
         </div>
       )}
     </form>
--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,5 +1,15 @@
+import { useSelector } from "react-redux";
+
 function Username() {
-  return <div className="hidden text-sm font-semibold md:block">Username</div>;
+  const username = useSelector((state) => state.user.username);
+
+  // add an onClick handler to log out?
+
+  if (!username) return null;
+
+  return (
+    <div className="hidden text-sm font-semibold md:block">{username}</div>
+  );
 }
 
 export default Username;
