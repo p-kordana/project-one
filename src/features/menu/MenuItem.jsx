92dbcba1eb2aee58f401b04fd1b1af8e0ee85958
p-kordana <pkordana422@gmail.com>
2024-05-07 14:23:42 -0400

    menu styling

--- src/features/menu/MenuItem.jsx
+++ src/features/menu/MenuItem.jsx
@@ -77,20 +77,20 @@ function MenuItem({ pizza }) {
 
                 {numInCart > 0 && (
                   <>
-                    <span className="border-2 border-stone-200 px-2 py-1 text-sm font-medium md:px-3 md:py-2 md:text-sm">
+                    <span className="rounded-md border-2 border-stone-200 px-1.5 py-1 text-xs font-medium md:px-2.5 md:py-1.5 md:text-sm">
                       {numInCart}
                     </span>
                     <Button
                       type="delete"
                       size="small"
-                      className="font-bold md:px-3 md:py-2 md:text-sm"
+                      className="font-bold md:px-2.5 md:py-1.5 md:text-sm"
                       onClick={handleRemove}
                     >
                       remove
                     </Button>
                     <Button
                       size="small"
-                      className="md:px-3 md:py-2 md:text-sm"
+                      className="md:px-2.5 md:py-1.5 md:text-sm"
                       onClick={handleAddMore}
                     >
                       Add another
